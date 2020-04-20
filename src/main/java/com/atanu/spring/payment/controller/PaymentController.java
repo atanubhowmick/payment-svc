2aaab357111ec2969a4ead6a884075e9dcf164f0
Atanu Bhowmick <mail2atanu007@gmail.com>
2020-04-20 08:54:51 +0530

    Profiles added

--- src/main/java/com/atanu/spring/payment/controller/PaymentController.java
+++ src/main/java/com/atanu/spring/payment/controller/PaymentController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +34,7 @@ public class PaymentController {
 	private BaseService<PaymentRequest, PaymentResponse> paymentService;
 	
 	@ApiOperation(value = "Make Payment", response = GenericResponse.class)
-	@GetMapping(value = "/make-payment", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/make-payment", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<GenericResponse<PaymentResponse>> getCartDetailsById(
 			@ApiParam(value = "Make Payment to pruchase products", required = true) @RequestBody PaymentRequest request) {
 		GenericResponse<PaymentResponse> response = new GenericResponse<>(paymentService.create(request));
